58227e45140004863d4951f70524080ad21e409e
Gurrenm3 <ThomasPMar@hotmail.com>
2020-09-20 15:47:18 -0700

    Adding functionality to Logger

--- NKHook6/NKHook6/Logger.cs
+++ NKHook6/NKHook6/Logger.cs
@@ -8,19 +8,39 @@ namespace NKHook6
 {
     public class Logger
     {
-        public static Logger instance;
-        public Logger()
+        public enum Level
         {
-            instance = this;
+            Normal,
+            Warning,
+            Error
         }
-        public void Log(string text, string messanger = "NKHook6")
+
+        public string Sender { get; set; } = System.Reflection.Assembly.GetExecutingAssembly().FullName;
+
+        private Logger instance;
+
+        public Logger Instance
+        {
+            get 
+            {
+                if (instance == null)
+                    instance = new Logger();
+                return instance; 
+            }
+            set { instance = value; }
+        }
+
+
+        public void Log(string text, int color = (int)ConsoleColor.Red, Level level = Level.Normal) => Log(text, color, level, Sender);
+
+        public static void Log(string text, int color = (int)ConsoleColor.Red, Level level, string sender = "")
         {
             Console.ForegroundColor = ConsoleColor.Green;
             Console.Write("(" + DateTime.Now + ") ");
             Console.ResetColor();
             Console.Write("[");
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.Write(messanger);
+            Console.ForegroundColor = (ConsoleColor)color;
+            Console.Write(sender);
             Console.ResetColor();
             Console.Write("] ");
             Console.WriteLine(text);
