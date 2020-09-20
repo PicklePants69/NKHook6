4549ff78a7001b3bc6201f1cd51f6f6aed0ed76f
DisabledMallis <mallisiscool@gmail.com>
2020-09-20 16:54:41 -0400

    Auto add assembly references to python scripts

--- NKHook6/NKHook6/Main.cs
+++ NKHook6/NKHook6/Main.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NKHook6.NKPython;
+using Assets.Scripts.Unity;
+using UnityEngine.Playables;
+using System.IO;
 
 namespace NKHook6
 {
@@ -18,11 +21,11 @@ namespace NKHook6
             logger.Log("CWD: " + Environment.CurrentDirectory);
 
             logger.Log("Setting up python...");
-            PyMain.Setup();
+            PyManager.Setup();
             logger.Log("Python set up!");
             string testScript = @"logger.Log('Hello from Python!');";
             logger.Log("Running test script...");
-            if (PyMain.Execute(testScript))
+            if (PyManager.Execute(testScript))
             {
                 logger.Log("Test success!");
             }
@@ -32,8 +35,15 @@ namespace NKHook6
                 return;
             }
 
+            string[] files = Directory.GetFiles("MelonLoader/Managed/");
+            foreach(string file in files)
+            {
+                string sanitized = file.Replace("MelonLoader/Managed/", "").Replace(".dll","");
+                Console.WriteLine("clr.AddReference('" + sanitized + "');");
+            }
+
             logger.Log("Running all scripts...");
-            PyMain.ExecuteAllScripts();
+            PyManager.ExecuteAllScripts();
             logger.Log("Scripts executed!");
 
             logger.Log("NKHook6 initialized");
