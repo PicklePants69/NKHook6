a1ddbe8ba9e384b74288341596510d127c45aba6
Matt <39469614+KosmicShovel@users.noreply.github.com>
2020-09-21 11:06:49 -0500

    Adding universal image support, Formatting weird because no class?

--- NKHook6/NKHook6/Api/Utilities/ImageUtils.cs
+++ NKHook6/NKHook6/Api/Utilities/ImageUtils.cs
@@ -4,13 +4,53 @@ using UnityEngine;
 
 namespace NKHook6.Api.Utilities
 {
-    //Taken from: https://github.com/SubnauticaModding/SMLHelper/blob/master/SMLHelper/Utility/ImageUtils.cs
 
-    /// <summary>
-    /// A collection of image loading utility methods that can create Unity objects from image files at runtime.
-    /// </summary>
-    public static class ImageUtils
-    {
+        public static Sprite LoadNewSprite(string FilePath, float PixelsPerUnit = 100.0f, SpriteMeshType spriteType = SpriteMeshType.Tight)
+        {
+
+            // Load a PNG or JPG image from disk to a Texture2D, assign this texture to a new sprite and return its reference
+
+            Texture2D SpriteTexture = LoadTexture(FilePath);
+            Sprite NewSprite = Sprite.Create(SpriteTexture, new Rect(0, 0, SpriteTexture.width, SpriteTexture.height), new Vector2(0, 0), PixelsPerUnit, 0, spriteType);
+
+            return NewSprite;
+        }
+
+        public static Sprite ConvertTextureToSprite(Texture2D texture, float PixelsPerUnit = 100.0f, SpriteMeshType spriteType = SpriteMeshType.Tight)
+        {
+            // Converts a Texture2D to a sprite, assign this texture to a new sprite and return its reference
+
+            Sprite NewSprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0, 0), PixelsPerUnit, 0, spriteType);
+
+            return NewSprite;
+        }
+
+        public static Texture2D LoadTexture(string FilePath)
+        {
+
+            // Load a PNG or JPG file from disk to a Texture2D
+            // Returns null if load fails
+
+            Texture2D Tex2D;
+            byte[] FileData;
+
+            if (File.Exists(FilePath))
+            {
+                FileData = File.ReadAllBytes(FilePath);
+                Tex2D = new Texture2D(2, 2);           // Create new "empty" texture
+
+                if (ImageConversion.LoadImage(Tex2D, FileData))           // Load the imagedata into the texture (size is set automatically)
+                    return Tex2D;                 // If data = readable -> return texture
+            }
+            return null;                     // Return null if load failed
+        }
+
+
+        //Taken from: https://github.com/SubnauticaModding/SMLHelper/blob/master/SMLHelper/Utility/ImageUtils.cs
+
+        /// <summary>
+        /// A collection of image loading utility methods that can create Unity objects from image files at runtime.
+        /// </summary>
         /// <summary>
         /// Creates a new <see cref="Texture2D" /> from an image file.
         /// </summary>
@@ -48,5 +88,4 @@ namespace NKHook6.Api.Utilities
 
             return null;
         }
-    }
 }
