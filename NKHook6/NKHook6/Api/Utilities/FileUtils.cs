b4c63e060605d02d66ba6186af24350babeb90ea
Thomas <52727840+gurrenm3@users.noreply.github.com>
2020-09-22 20:44:22 -0700

    Create FileUtils.cs

---
+++ NKHook6/NKHook6/Api/Utilities/FileUtils.cs
@@ -0,0 +1,9 @@
+public static string ReadFileContentTest(FileStream str)
+{
+  //Not sharing the specifics of how it reads
+}
+
+public static bool WriteFileContentTest(string json, FileStream outputFile)
+{
+  //Not sharing the specifics of how it writes
+}
