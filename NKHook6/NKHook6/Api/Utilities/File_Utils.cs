cd32ce50a736a48ccf2473c1367a8799d0ec312d
Thomas <52727840+gurrenm3@users.noreply.github.com>
2020-09-22 20:50:30 -0700

    Update File_Utils.cs

--- NKHook6/NKHook6/Api/Utilities/File_Utils.cs
+++ NKHook6/NKHook6/Api/Utilities/File_Utils.cs
@@ -1,8 +1,14 @@
+/// <summary>
+/// Read data from a file
+/// </summary>
 public static string ReadFileContentTest(FileStream str)
 {
   //Not sharing the specifics of how it reads
 }
 
+/// <summary>
+/// Save data to a file
+/// </summary>
 public static bool WriteFileContentTest(string json, FileStream outputFile)
 {
   //Not sharing the specifics of how it writes
