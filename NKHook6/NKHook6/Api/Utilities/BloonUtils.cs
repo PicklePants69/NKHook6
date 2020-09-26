a91931ce194bf97680dfeb1d671daff4d6f0e69d
Gurrenm3 <ThomasPMar@hotmail.com>
2020-09-26 07:54:11 -0700

    Update BloonUtils.cs

--- NKHook6/NKHook6/Api/Utilities/BloonUtils.cs
+++ NKHook6/NKHook6/Api/Utilities/BloonUtils.cs
@@ -12,9 +12,78 @@ namespace NKHook6.Api.Utilities
     {
         public static List<Bloon> BloonsOnMap = new List<Bloon>();
 
+        /// <summary>
+        /// Get a list of all of the default bloon ids. Doesn't get custom bloons
+        /// </summary>
+        /// <returns></returns>
+        public static List<string> GetAllBloonTypes()
+        {
+            var allBloonTypes = new List<string>();
+            foreach (var item in EnumUtils.GetValues<DefaultBloonIds>())
+                allBloonTypes.Add(item.ToString());
 
+            return allBloonTypes;
+        }
+
+
+        /// <summary>
+        /// Get the next strongest bloon. Ex: the next strongest bloon after Red is Red Regrow
+        /// </summary>
+        /// <param name="bloonId">The bloon id of the current bloon. Ex: Red</param>
+        /// <param name="allowCamo">Is it okay if the next bloon is a camo bloon. Ex: Red => RedCamo</param>
+        /// <param name="allowFortified">Is it okay if the next bloon is a Fortified bloon. Ex: Red => RedFortified</param>
+        /// <param name="allowRegrow">Is it okay if the next bloon is a Regrow bloon. Ex: Red => RedRegrow</param>
+        /// <returns>The next strongest bloon</returns>
+        public static BloonModel GetNextStrongestBloon(DefaultBloonIds bloonId, bool allowCamo = true, 
+            bool allowFortified = true, bool allowRegrow = true) => GetNextStrongestBloon(bloonId.ToString());
+
+
+        /// <summary>
+        /// Get the next strongest bloon. Ex: the next strongest bloon after Red is Red Regrow
+        /// </summary>
+        /// <param name="bloonId">The bloon id of the current bloon. Ex: Red</param>
+        /// <param name="allowCamo">Is it okay if the next bloon is a camo bloon. Ex: Red => RedCamo</param>
+        /// <param name="allowFortified">Is it okay if the next bloon is a Fortified bloon. Ex: Red => RedFortified</param>
+        /// <param name="allowRegrow">Is it okay if the next bloon is a Regrow bloon. Ex: Red => RedRegrow</param>
+        /// <returns>The next strongest bloon</returns>
+        public static BloonModel GetNextStrongestBloon(string bloonId, bool allowCamo = true,
+            bool allowFortified = true, bool allowRegrow = true)
+        {
+            var allBloonTypes = GetAllBloonTypes();
+
+            string nextBloon = bloonId;
+            int max = allBloonTypes.Count - 2; //Subtracting 2 to avoid testbloon
+            for (int i = 0; i < max; i++)
+            {
+                if (bloonId.ToLower() != allBloonTypes[i].ToLower())
+                    continue;
+                nextBloon = allBloonTypes[i];
+            }
+
+            if (nextBloon.Contains("Camo") && allowCamo == false)
+                nextBloon = nextBloon.Replace("Camo", "");
+            if (nextBloon.Contains("Fortified") && allowFortified == false)
+                nextBloon = nextBloon.Replace("Fortified", "");
+            if (nextBloon.Contains("Regrow") && allowRegrow == false)
+                nextBloon = nextBloon.Replace("Regrow", "");
+
+            return GetBloon(nextBloon);
+        }
+
+
+        /// <summary>
+        /// Get the BloonModel of the bloonId you enter
+        /// </summary>
+        /// <param name="bloonId">The ID of the bloon you want</param>
+        /// <returns></returns>
         public static BloonModel GetBloon(DefaultBloonIds bloonId) => GetBloon(bloonId.ToString());
 
+
+        /// <summary>
+        /// Get the BloonModel of the bloonId you enter
+        /// </summary>
+        /// <param name="bloonId">The ID of the bloon you want</param>
+        /// <returns></returns>
         public static BloonModel GetBloon(string bloonId)
         {
             BloonModel result = null;
