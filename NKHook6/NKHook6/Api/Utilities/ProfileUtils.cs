6d8f213df477d84ae142a35162e4a3c0084a7d7b
Gurrenm3 <ThomasPMar@hotmail.com>
2020-10-04 11:05:03 -0700

    Added TowerSelectionMenu stuff and Lock/Unlock towers for TowerSelectionMenu

---
+++ NKHook6/NKHook6/Api/Utilities/ProfileUtils.cs
@@ -0,0 +1,62 @@
+﻿using Assets.Scripts.Models.Profile;
+using Assets.Scripts.Models.Towers;
+using Assets.Scripts.Models.TowerSets;
+using Harmony;
+using Il2CppSystem.Collections.Generic;
+
+namespace NKHook6.Api.Utilities
+{
+    public class ProfileUtils
+    {
+        public static List<string> UnlockTheseTowers;
+        public static List<string> LockTheseTowers;
+    }
+
+    [HarmonyPatch(typeof(ProfileModel), "Validate")] // this method is called after the profile data is parsed, hence why it's used to modify said profile data
+    internal class ProfileModel_Patch
+    {
+        [HarmonyPostfix]
+        internal static void Postfix(ProfileModel __instance)
+        {
+            if ((ProfileUtils.UnlockTheseTowers == null || ProfileUtils.UnlockTheseTowers.Count == 0) && 
+                (TowerUtils.AddTheseTowersToList == null || TowerUtils.AddTheseTowersToList.Count == 0))
+                return;
+
+            List<string> unlockedTowers = __instance.unlockedTowers;
+
+            if (TowerUtils.AddTheseTowersToList != null && TowerUtils.AddTheseTowersToList.Count > 0)
+            {
+                foreach (var item in TowerUtils.AddTheseTowersToList)
+                {
+                    if (unlockedTowers.Contains(item.key.towerId))
+                        continue;
+
+                    unlockedTowers.Add(item.key.towerId);
+                }
+            }
+
+
+            if (ProfileUtils.UnlockTheseTowers != null && ProfileUtils.UnlockTheseTowers.Count > 0)
+            {
+                foreach (var item in ProfileUtils.UnlockTheseTowers)
+                {
+                    if (unlockedTowers.Contains(item))
+                        continue;
+
+                    unlockedTowers.Add(item);
+                }
+            }
+
+            if (ProfileUtils.LockTheseTowers != null && ProfileUtils.LockTheseTowers.Count > 0)
+            {
+                foreach (var item in ProfileUtils.LockTheseTowers)
+                {
+                    if (!unlockedTowers.Contains(item))
+                        continue;
+
+                    unlockedTowers.Remove(item);
+                }
+            }
+        }
+    }
+}
--- NKHook6/NKHook6/Api/Utilities/TowerUtils.cs
+++ NKHook6/NKHook6/Api/Utilities/TowerUtils.cs
@@ -1,9 +1,15 @@
 ﻿using Assets.Scripts.Models.Towers;
+using Assets.Scripts.Models.TowerSets;
+using Assets.Scripts.Simulation.Input;
 using Assets.Scripts.Simulation.Towers;
 using Assets.Scripts.Unity;
+using Assets.Scripts.Unity.UI_New.InGame;
+using Harmony;
 using NKHook6.Api.Enums;
-using System.Collections.Generic;
+using Il2CppSystem.Collections.Generic;
+using System.Linq;
 using System.Runtime.InteropServices;
+using Assets.Scripts.Models.Profile;
 
 namespace NKHook6.Api.Utilities
 {
@@ -13,22 +19,78 @@ namespace NKHook6.Api.Utilities
         /// A list of all towers currently on the map. Updated when a tower is initialised or destroyed
         /// </summary>
         public static List<Tower> TowersOnMap = new List<Tower>();
-        /*public static string GetTowerName(DefaultTowerIds baseId, [Optional]int tier1, [Optional]int tier2, [Optional]int tier3)
-            => GetTower(baseId, tier1, tier2, tier3).name;
 
-        public static TowerModel GetTower(DefaultTowerIds baseId, [Optional]int tier1, [Optional]int tier2, [Optional]int tier3)
-            => GetTower(baseId.ToString(), tier1, tier2, tier3);*/
+        public static List<TowerDetailsModel> AllTowersInTheGame = new List<TowerDetailsModel>();
+
+        internal static Dictionary<ShopTowerDetailsModel, int> AddTheseTowersToList;
+
+        public static TowerInventory TowerInventory;
+
+
 
         public static TowerModel GetTower(TowerType baseId, [Optional]int tier1, [Optional]int tier2, [Optional]int tier3)
             => GetTower(baseId.ToString(), tier1, tier2, tier3);
 
-        public static TowerModel GetTower(string baseId, [Optional]int tier1, [Optional]int tier2, [Optional]int tier3)
-        {
-            string towerName = baseId;
-            /*if (baseId == DefaultTowerIds.BallOfLight__Tower.ToString())
-                towerName = towerName.Replace("__", "-");*/
+        public static TowerModel GetTower(string baseId, [Optional]int tier1, [Optional]int tier2, [Optional]int tier3) 
+            => Game.instance.model.GetTower(baseId, tier1, tier2, tier3);
 
-            return Game.instance.model.GetTower(towerName, tier1, tier2, tier3);
+
+
+        //Thanks to BowDown for creating the AddTowerToInventory methods
+        public static void AddToTowerSelection(ShopTowerDetailsModel details, TowerType insertBefore)
+            => AddToTowerSelection(details, insertBefore.ToString(), AllTowersInTheGame);
+
+        public static void AddToTowerSelection(ShopTowerDetailsModel details, string insertBefore)
+            => AddToTowerSelection(details, insertBefore, AllTowersInTheGame);
+
+        public static void AddToTowerSelection(ShopTowerDetailsModel details, int index) =>
+            AddToTowerSelection(details, index, AllTowersInTheGame);
+
+
+        public static void AddToTowerSelection(ShopTowerDetailsModel details, TowerType insertBefore, List<TowerDetailsModel> allTowersInTheGame)
+            => AddToTowerSelection(details, insertBefore.ToString(), allTowersInTheGame);
+
+        public static void AddToTowerSelection(ShopTowerDetailsModel details, string insertBefore, List<TowerDetailsModel> allTowersInTheGame)
+        {
+            TowerDetailsModel towerAfter = allTowersInTheGame.ToArray().FirstOrDefault(tower => tower.towerId == insertBefore);
+            AddToTowerSelection(details, allTowersInTheGame.IndexOf(towerAfter), allTowersInTheGame);
+        }
+
+       
+        public static void AddToTowerSelection(ShopTowerDetailsModel details, int index, List<TowerDetailsModel> allTowersInTheGame)
+        {
+            if (TowerInventory == null)
+            {
+                if (AddTheseTowersToList == null)
+                    AddTheseTowersToList = new Dictionary<ShopTowerDetailsModel, int>();
+
+                AddTheseTowersToList.Add(details, index);
+                return;
+            }
+
+            allTowersInTheGame.Insert(index, details);
+        }
+    }
+
+    [HarmonyPatch(typeof(TowerInventory), "Init")] // this method tells the game to create buttons for a given list of towers, allTowersInTheGame, which we modify here
+    internal class TowerInit_Patch
+    {
+        [HarmonyPrefix]
+        internal static bool Prefix(TowerInventory __instance, ref List<TowerDetailsModel> allTowersInTheGame)
+        {
+            TowerUtils.AllTowersInTheGame = allTowersInTheGame;
+            TowerUtils.TowerInventory = __instance;
+
+            if (TowerUtils.AddTheseTowersToList == null || TowerUtils.AddTheseTowersToList.Count == 0)
+                return true;
+
+            foreach (var item in TowerUtils.AddTheseTowersToList)
+            {
+                if (!allTowersInTheGame.Contains(item.key))
+                    TowerUtils.AddToTowerSelection(item.Key, item.Value, allTowersInTheGame);
+            }
+
+            return true;
         }
     }
 }
